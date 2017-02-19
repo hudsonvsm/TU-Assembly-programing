1364a32b58672f29d7707eee165a29599acd4e8d
Valentin Mladenov <hudsonvsm@gmail.com>
2017-02-20 00:11:18 +0200

    array search
    find INT in NULL terminated array, if INT not found append it at last
    index and move the NULL after it.

---
+++ 16.array.search.asm
@@ -0,0 +1,19 @@
+; М е едномерен масив от байтове, завършващ с байта 0. Напишете
+; процедура, която добавя в края му символи от регистъра AL, ако този символ
+; липсва в масива. Новият масив също да завършва с нулев байт.
+
+.DATA   ; VARIABLES IN HEAP
+        ARR     DB      1,2,3,4,5,6,7,8,9,10,0
+.CODE
+MAIN    PROC
+        MOV     BX,0            ; BX = 0
+SRCH:
+        CMP     AL,ARR[BX]      ; AL == ARR[INDEX]
+        JE      BREAK           ; IF TRUE RETURN
+        INC     BX              ; BX = BX + 1 OR BX++
+        CMP     ARR[BX-1],0     ; BX-1 == 0 (MINUS 1 BECAUSE ALLREADY INCREMENTED)
+        JNE     SRCH            ; IF NOT TRUE GO TO SRCH
+        MOV     ARR[BX],AL      ; IF INT NOT FOUND LAST INDEX == AL
+        MOV     ARR[BX+1],0     ; MAKE NEW NULL POINTER AFTER LAST INDEX
+BREAK:
+    RET
